3f7462cf5c168592e0234e32aab4af0dd2bb555d
Tóth Márk Zoltán <zollee60@gmail.com>
2021-05-31 01:01:14 +0200

    Application configuration

--- app.ts
+++ app.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import { ImageController } from './controllers/ImageController';
 
 const app: express.Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/uploads', express.static('public/images'));
+app.use('/api', ImageController);
+app.use('/', express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Well done!');
