8cb156cac8e646bfca04c65a8268d32cf99ec9b0
Tóth Márk Zoltán <zollee60@gmail.com>
2021-06-04 09:06:28 +0200

    Project configuration update

--- app.ts
+++ app.ts
@@ -1,18 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import { ImageController } from './controllers/ImageController';
 
 const app: express.Application = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('public/images'));
 app.use('/api', ImageController);
 app.use('/', express.static('public'));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.get('/', (req, res) => {
     res.send('Well done!');
 })
 
-app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
+app.listen(4000, () => {
+    console.log('The application is listening on port 4000!');
 })
